d244376d6f9a5b0f51a3b0e2b8e3bd0baaf35d28
Cony Erazo <conyerazo@MacBook-Pro-de-Cony.local>
2022-03-06 09:41:02 -0600

    Adding service test

--- src/main/java/com/elioth/awesome/message/controller/request/UserRequest.java
+++ src/main/java/com/elioth/awesome/message/controller/request/UserRequest.java
@@ -2,13 +2,15 @@ package com.elioth.awesome.message.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class UserRequest {
 
     private final String username;
 
     @JsonCreator
-    public UserRequest(@JsonProperty("username") final String username) {
+    public UserRequest(@JsonProperty("username") @NotBlank @NotNull final String username) {
         this.username = username;
     }
 
---
+++ src/test/java/com/elioth/awesome/message/service/UserServiceTest.java
@@ -0,0 +1,56 @@
+package com.elioth.awesome.message.service;
+
+import com.elioth.awesome.message.controller.request.UserRequest;
+import com.elioth.awesome.message.repository.UserRepository;
+import com.elioth.awesome.message.repository.entity.UserEntity;
+import com.elioth.awesome.message.resource.User;
+import java.time.Instant;
+import java.util.Optional;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    private static final Random RANDOM_ID_GENERATOR = new Random(10);
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void testCreateUserWithNoExistingUser() {
+        final UserRequest userRequest = new UserRequest("test");
+        final UserEntity user = new UserEntity();
+        user.setUsername(userRequest.getUsername());
+        when(userRepository.save(eq(user))).thenReturn(getStoredUser(user));
+        final User newUser = userService.createUser(userRequest);
+        assertThat(newUser).isNotNull();
+        assertThat(newUser.getUsername()).isEqualTo(userRequest.getUsername());
+    }
+
+    @Test
+    public void testCreateUserWithExistingUserName() {
+        final UserRequest userRequest = new UserRequest("test");
+        final UserEntity user = new UserEntity();
+        user.setUsername(userRequest.getUsername());
+        when(userRepository.findByUsername(eq(userRequest.getUsername()))).thenReturn(Optional.of(getStoredUser(user)));
+        final User newUser = userService.createUser(userRequest);
+        assertThat(newUser).isNull();
+    }
+
+    private UserEntity getStoredUser(UserEntity user) {
+        final UserEntity storedUser =  new UserEntity();
+        storedUser.setUsername(user.getUsername());
+        storedUser.setCreatedAt(Instant.now());
+        storedUser.setId(RANDOM_ID_GENERATOR.nextLong());
+        return storedUser;
+    }
+}
