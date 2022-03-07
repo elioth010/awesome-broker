8da1d97b9c2d782cf57a40ee7c24fc559bd8ae33
Elioth Rivera <elioth010@gmail.com>
2022-03-06 20:06:43 -0600

    Fix swagger documents

--- src/main/java/com/elioth/awesome/message/controller/MessagingController.java
+++ src/main/java/com/elioth/awesome/message/controller/MessagingController.java
@@ -3,6 +3,12 @@ package com.elioth.awesome.message.controller;
 import com.elioth.awesome.message.controller.exception.NoCircularMessageAllowedException;
 import com.elioth.awesome.message.controller.request.MessageSendRequest;
 import com.elioth.awesome.message.producers.MessageDeliveryProducer;
+import com.elioth.awesome.message.resource.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.Objects;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +28,12 @@ public class MessagingController {
         this.messagingProducer = messagingProducer;
     }
 
-    @PostMapping(value = "/messages/send-message", consumes = APPLICATION_JSON_VALUE)
-    @ResponseBody
+    @Operation(summary = "Create request to send message", description = "Message delivery system it will accept messages, but it will be processed async for delivery", tags = {"message"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Message delivery request was accepted ", content = @Content),
+            @ApiResponse(responseCode = "406", description = "Messages can't be sent to the same user", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Validations did not succeed", content = @Content)})
+    @PostMapping(value = "/messages/send-message", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> sendMessage(@Valid @RequestBody final MessageSendRequest sendMessageRequest) {
         if (Objects.equals(sendMessageRequest.getFrom(), sendMessageRequest.getTo())) {
             throw new NoCircularMessageAllowedException();
